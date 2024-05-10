9e3251361b30999d9b0b4161fce4f95f0c3f452b
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2024-05-10 16:27:04 +0700

    feat: basic directory search

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/LiddleChild/findr/internal/parameters"
+	"github.com/LiddleChild/findr/utils"
 )
 
 /*
@@ -36,4 +39,43 @@ func main() {
 	}
 
 	fmt.Println(query, args)
+
+	type FileStack struct {
+		path  string
+		depth int
+	}
+
+	st := utils.NewStack[FileStack]()
+	st.Push(FileStack{
+		path: args.WorkingDirectory,
+		depth: 0,
+	})
+
+	for st.Size() > 0 {
+		top := st.Top()
+		st.Pop()
+
+		if top.depth > args.MaxDepth {
+			continue
+		}
+
+		entries, err := os.ReadDir(top.path)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
+		for _, e := range entries {
+			if strings.Contains(e.Name(), query) {
+				fmt.Println(filepath.Join(top.path, e.Name()))
+			}
+
+			if e.IsDir() {
+				st.Push(FileStack{
+					path: filepath.Join(top.path, e.Name()),
+					depth: top.depth + 1,
+				})
+			}
+		}
+	}
 }
